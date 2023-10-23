6258abdc0337bec0c341f2b1e5494292c3416fe6
TheRealAGBOOLA <agboolasamson602@gmail.com>
2023-10-23 15:13:21 +0100

    fixed a bug caused by space in the id

--- script.js
+++ script.js
@@ -32,23 +32,23 @@ const valBtn = document.querySelectorAll("input");
 const lightMode = document.getElementById("lightmode");
 const darkMode = document.getElementById("darkmode");
 
-//window.addEventListener("load", () => {
-//  document.getElementById("light").style.background = "rgba(255,255,255,0.6)";
-//});
+window.addEventListener("load", () => {
+    document.getElementById("light").style.background = "rgba(255,255,255,0.6)";
+});
 console.log(valBtn);
 themeBtn.addEventListener("click", (e) => {
     if (e.target.id === "dark") {
         e.target.style.background = "rgba(255, 255, 255, 0.6)";
         e.target.previousElementSibling.style.background = "rgba(255, 255, 255, 0.1)";
-        lightMode.style.display = "none";
-        darkMode.style.display = "block";
+        // lightMode.style.display = "none";
+        //darkMode.style.display = "block";
         alert("dark mode will be actived");
     } else {
 
         e.target.style.background = "rgba(255, 255, 255, 0.6)";
         e.target.nextElementSibling.style.background = "rgba(255, 255, 255, 0.1)";
-        lightMode.style.display = "block";
-        darkMode.style.display = "none";
+        //lightMode.style.display = "block";
+        //darkMode.style.display = "none";
 
         alert("light mode  will be actived");
     }
